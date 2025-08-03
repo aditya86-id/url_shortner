4d0929d33cce4bc2a4e156f43a6ab51a6b3705c1
adityakumartiwary <adityatiwary888@gmail.com>
2025-08-03 10:59:37 +0530

    first commit

--- src/urlshort/models.py
+++ src/urlshort/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class SHortURL(models.Model):
+    original_url = models.URLField(max_length=700)
+    time_date_created = models.DateTimeField()
+    short_url = models.CharField(max_length=100)
+    
